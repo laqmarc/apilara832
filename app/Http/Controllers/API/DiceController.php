fda2f8a2695aef184f654f31b70ddb728d3f4656
laqmarc <laqmarc@gmail.com>
2022-09-13 21:55:42 +0200

    dice, controller and routes

--- app/Http/Controllers/API/DiceController.php
+++ app/Http/Controllers/API/DiceController.php
@@ -4,38 +4,123 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\Dice;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+
+use function GuzzleHttp\Promise\all;
 
 class DiceController extends Controller
 {
     public function throwDice ($id)
     {
 
-            // $id=3;
-            $diceA = rand(1,6);
-            $diceB = rand(1,6);
-            $twodices = $diceA + $diceB;
-        
-            if($twodices == 7) 
-            {
-                $result = 1;
-            }else 
-            {
-                $result = 0;
-            }
             
-            Dice::create([
-            "diceA" => $diceA,
-            "diceB" => $diceB,
-            "result" => $result,
-            "user_id" => $id
-            ])->where('user_id', '=', $id)->get();
-        
-            if($result == 1){
-            return response(["message" => "Nice you WIN. $diceA and $diceB is:  $twodices."]);
-            }
-            else{
-            return response(["message" => "Lose!! $diceA and $diceB is: $twodices."]);
-            }
-        }
+        $diceA = rand(1,6);
+        $diceB = rand(1,6);
+        $twodices = $diceA + $diceB;
     
+        if($twodices !== 7) 
+        {
+            $result = 0;
+        }else 
+        {
+            $result = 1;
+        }
+        
+        Dice::create([
+        "diceA" => $diceA,
+        "diceB" => $diceB,
+        "result" => $result,
+        "user_id" => $id
+        ])->where('user_id', '=', $id)->get();
+    
+        if($result !== 1){
+
+            return response(["message" => "Lose!! $diceA and $diceB is: $twodices."]);
+        }
+        else{
+            
+            return response(["message" => "Nice you WIN. $diceA and $diceB is:  $twodices."]);
+
+        }
+    }
+
+    public function allGames ($id){
+        $playergames = Dice::where('user_id', '=', $id)->first('id');
+
+        if($playergames !== null)
+        {
+            $throws = Dice::where('user_id', $id)->get();
+            return response(["message" => "throws:", $throws]);
+
+        }elseif($playergames == null)
+        {
+            return response(["message" => "No throws"]);
+        }
+    }
+
+    public function deleteAllGames ($id){
+        $playergames = Dice::where('user_id', '=', $id)->first('id');
+
+        if($playergames !== null)
+        {
+            Dice::where('user_id', $id)->delete();
+            return response(["message" => "all Games deleted."]);
+
+        }else{
+            return response(["message" => "no games to delete, play please"]);
+        }
+    }
+
+    public function stats(){
+        $stats  = DB::table('dices')        
+        ->join('users', 'dices.user_id', '=', 'users.id')
+        ->selectRaw('users.name as users, 
+         sum(dices.result = 1)*100/count(dices.result) as winrate')  
+        ->orderby('winrate', 'desc')
+        ->groupby('users')
+        ->get();
+
+        return response(["User win rate" => $stats]);
+    }
+
+    public function rankingPlayers(){
+
+        $allplayerRanking = DB::table('dices')
+        ->join('users', 'dices.user_id', '=', 'users.id')
+        ->selectRaw('sum(dices.result = 1)/count(DISTINCT dices.id)/count(DISTINCT users.id)*100 as result')
+        ->get();
+ 
+        return response(["All players win rate" => $allplayerRanking]);
+
+    }
+
+    public function rankingPlayersLoser(){
+        $playerRankingLoser = DB::table('dices')
+        ->join('users', 'dices.user_id', '=', 'users.id')
+        ->selectRaw('users.name as users, 
+         sum(dices.result = 1)*100/count(dices.result) as winrate')  
+        ->orderby('winrate')
+        ->groupby('users')
+        ->limit(1)
+        ->get();
+ 
+        return response(["The worst player" => $playerRankingLoser]);
+
+    }
+
+    public function rankingPlayersWinner(){
+        $playerRankingWinner = DB::table('dices')
+        ->join('users', 'dices.user_id', '=', 'users.id')
+        ->selectRaw('users.name as users, 
+         sum(dices.result = 1)*100/count(dices.result) as winrate')  
+        ->orderby('winrate', 'desc')
+        ->groupby('users')
+        ->limit(1)
+        ->get();
+ 
+        return response(["The best player" => $playerRankingWinner]);
+
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -19,28 +19,38 @@ use App\Http\Controllers\API\DiceController;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+//PLAYER
 // POST /players : crea un jugador/a.
 Route::post('players', [AuthController::class, 'register']);
-// POST /players/{id}/games/ : un jugador/a específic realitza una tirada dels daus.
-Route::post('/players/{id}/games', [DiceController::class, 'throwDice']);
-
-
-
-
 // LOGIN
 Route::post('login', [AuthController::class, 'login']);
-
-
 //LOGOUT
 Route::post('/logout', [AuthController::class, 'logout']);
 
-
 // PUT /players/{id} : modifica el nom del jugador/a.
+
+
+
+
+//GAME
+
 // POST /players/{id}/games/ : un jugador/a específic realitza una tirada dels daus.
-// DELETE /players/{id}/games: elimina les tirades del jugador/a.
-// GET /players: retorna el llistat de tots els jugadors/es del sistema amb el seu percentatge mitjà d’èxits 
+Route::post('/players/{id}/games', [DiceController::class, 'throwDice']);
 // GET /players/{id}/games: retorna el llistat de jugades per un jugador/a.
+Route::get('/players/{id}/games', [DiceController::class, 'allGames']);
+// DELETE /players/{id}/games: elimina les tirades del jugador/a.
+Route::delete('/players/{id}/games', [DiceController::class, 'deleteAllGames']);
+
+//STATS
+// GET /players: retorna el llistat de tots els jugadors/es del sistema amb el seu percentatge mitjà d’èxits 
+Route::get('/players', [DiceController::class, 'stats']);
 // GET /players/ranking: retorna el rànquing mitjà de tots els jugadors/es del sistema. És a dir, el percentatge mitjà d’èxits.
+Route::get('players/ranking',[DiceController::class, 'rankingPlayers']);
 // GET /players/ranking/loser: retorna el jugador/a amb pitjor percentatge d’èxit.
+Route::get('players/ranking/loser',[DiceController::class, 'rankingPlayersLoser']);
 // GET /players/ranking/winner: retorna el jugador/a amb millor percentatge d’èxit.
+Route::get('players/ranking/winner',[DiceController::class, 'rankingPlayersWinner']);
+
+
+
+
