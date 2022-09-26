cdcca5bdaee05f7a556e390cb9c8bb97dfc92b11
laqmarc <laqmarc@gmail.com>
2022-09-26 20:54:19 +0200

    test works!!!

--- routes/api.php
+++ routes/api.php
@@ -28,49 +28,33 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 //PLAYER
-// POST /players : crea un jugador/a.
-Route::post('players', [AuthController::class, 'register']);
-// LOGIN
+// POST /players : crea un jugador/a. - test ok
+Route::post('players', [AuthController::class, 'register'])->name('register');
+// LOGIN - test ok
 Route::post('login', [AuthController::class, 'login']);
-//LOGOUT
-// Route::post('/logout', [AuthController::class, 'logout']);
-//logout with middleware
-// Route::middleware('auth:api')->get('/logout', [AuthController::class, 'logout']);
-
-// UPDATE
-// PUT /players/{id} : modifica el nom del jugador/a.
-// Route::put('/players/{id}', [AuthController::class, 'update']);
-// Route::middleware('auth:api')->put('/players/{id}', [AuthController::class, 'updateName']);
-
-
-
-
-
-
 
 
 Route::middleware('auth:api')->group(function () {
 //GAME
-
-// POST /players/{id}/games/ : un jugador/a específic realitza una tirada dels daus.
-Route::post('/players/{id}/games', [DiceController::class, 'throwDice']);
-// GET /players/{id}/games: retorna el llistat de jugades per un jugador/a.
-Route::get('/players/{id}/games', [DiceController::class, 'allGames']);
-// DELETE /players/{id}/games: elimina les tirades del jugador/a.
-Route::delete('/players/{id}/games', [DiceController::class, 'deleteAllGames']);
+// POST /players/{id}/games/ : un jugador/a específic realitza una tirada dels daus. - test ok
+Route::post('/players/{id}/games', [DiceController::class, 'throwDice'])->name('throwDice');
+// GET /players/{id}/games: retorna el llistat de jugades per un jugador/a. - test ok
+Route::get('/players/{id}/games', [DiceController::class, 'allGames'])->name('allGames');
+// DELETE /players/{id}/games: elimina les tirades del jugador/a. - test ok
+Route::delete('/players/{id}/games', [DiceController::class, 'deleteAllGames'])->name('deleteAllGames');
 
 //STATS
-// GET /players: retorna el llistat de tots els jugadors/es del sistema amb el seu percentatge mitjà d’èxits 
-Route::get('/players', [DiceController::class, 'stats']);
+// GET /players: retorna el llistat de tots els jugadors/es del sistema amb el seu percentatge mitjà d’èxits - test ok
+Route::get('/players', [DiceController::class, 'stats'])->name('stats');
 // GET /players/ranking: retorna el rànquing mitjà de tots els jugadors/es del sistema. És a dir, el percentatge mitjà d’èxits.
-Route::get('players/ranking',[DiceController::class, 'rankingPlayers']);
-// GET /players/ranking/loser: retorna el jugador/a amb pitjor percentatge d’èxit.
-Route::get('players/ranking/loser',[DiceController::class, 'rankingPlayersLoser']);
+Route::get('players/ranking',[DiceController::class, 'rankingPlayers'])->name('rankingPlayers');
+// GET /players/ranking/loser: retorna el jugador/a amb pitjor percentatge d’èxit. - test ok
+Route::get('players/ranking/loser',[DiceController::class, 'rankingPlayersLoser'])->name('rankingPlayersLoser');
 // GET /players/ranking/winner: retorna el jugador/a amb millor percentatge d’èxit.
-Route::get('players/ranking/winner',[DiceController::class, 'rankingPlayersWinner']);
-// PUT /players/{id} : modifica el nom del jugador/a.
-Route::put('/players/{id}',[AuthController::class, 'updateName']);
-//Logout
-Route::post('/logout', [AuthController::class, 'logout']);
+Route::get('players/ranking/winner',[DiceController::class, 'rankingPlayersWinner'])->name('rankingPlayersWinner');
+// PUT /players/{id} : modifica el nom del jugador/a. -test ok
+Route::put('/players/{id}',[AuthController::class, 'updateName'])->name('updateName');
+//Logout - test ok
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 });
---
+++ tests/Feature/AUserTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\User;
+use Laravel\Passport\Passport;
+use Tests\TestCase;
+
+class AUserTest extends TestCase
+{
+    use WithFaker;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    /** @test */
+    public function testRegisterUserOnDatabase (){
+
+        $this->artisan('passport:install');
+        $this->withoutExceptionHandling();
+        $response = $this->postJson('api/players', [
+                'name' => $this->faker->name,
+                'email' => $this->faker->email,
+                'password' => '12341234',
+                'password_confirmation' => '12341234',
+                'role' => 'user'
+        ]);
+
+        $response->assertSuccessful();
+        $user = User::first();
+        // $this->assertEquals('m', $user->name);
+        // $this->assertEquals('ma@m.com', $user->email);
+        $this->assertDatabaseHas('users', $user->toArray());
+    }
+
+    /** @test */
+    public function testUserCanLogin(){      
+        $user = User::factory()->make();
+        $params = ['email' => 'marc@quexulo.cat', 'password' => '12341234',];
+        $response =  $this->post( '/api/login', $params);
+        $isuser = $user ? true : false;
+        $this->assertTrue($isuser);
+    }
+    
+    /** @test */
+    public function testUserCanLogout($user = null){
+        $user = $user ?: User::factory()->create();
+        $user = Passport::actingAs($user);
+        $response = $this->actingAs($user, 'api')->json('POST', '/api/logout');
+        $response->assertOk();
+
+    }
+
+    public function testUserCanUpdate(){
+
+        $this->artisan('passport:install');    
+        $user = User::factory()->create();
+        $user = Passport::actingAs($user);
+        $response = $this->actingAs($user, 'api')->put(route('updateName', $user->id),
+        ['name' => 'ma',
+         'email' => 'maa@m.com'  
+        ]);
+        $this->assertAuthenticated();
+        $response->assertOk();
+        $this->assertDatabaseHas('users', ['name' => 'ma']);
+    }
+    
+}
